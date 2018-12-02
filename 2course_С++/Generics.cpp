cd79fa32798f79bb10ce28748020c4a698673ece
Kurguzov Vladimir <31987030+saxakiil@users.noreply.github.com>
2018-12-02 21:13:33 +0300

    Update Generics.cpp

--- 2course_С++/Generics.cpp
+++ 2course_С++/Generics.cpp
@@ -1,74 +1,90 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-template <class T>
-class BinTree{
+
+template<class T>
+class Tree {
 private:
-    class Node{
-        friend BinTree;
-    private:
-        Node *left, *right;
-        T value;
+    struct NodeTree {
     public:
-        Node(T val){
-            this->left = nullptr;
-            this->right = nullptr;
-            this->value = val;
-        }
+        NodeTree *left;
+        NodeTree *right;
+        T data;
     };
-    Node *root;
-
-    void AddTo(Node &node, T val) {
-        if (val < node.value) {
-            if (node.left == nullptr) {
-                node.left = new Node(val);
-            } else {
-                AddTo(*node.left, val);
-            }
-        }
-        else {
-            if (node.right == nullptr) {
-                node.right = new Node(val);
-            } else {
-                AddTo(*node.right, val);
-            }
+    NodeTree *root = NULL;
+    int sizeTree = 0;
+    vector<T> el;
+    NodeTree *GetNewNode(T data) {
+        NodeTree *newNode = new NodeTree;
+        newNode->data = data;
+        newNode->left = NULL;
+        newNode->right = NULL;
+        return newNode;
+    }
+    void shower(NodeTree *&Tree){
+        if (Tree != NULL){
+            shower(Tree->left);
+            el.push_back(root->data);
+            cout<<Tree->data << endl;
+            shower(Tree->right);
         }
     }
-
+    NodeTree* ToAdder(NodeTree* root,T data) {
+        if (root == NULL) {
+            root = GetNewNode(data);
+        } else if (data <= root->data) {
+            root->left = ToAdder(root->left, data);
+        } else {
+            root->right = ToAdder(root->right, data);
+        }
+        return root;
+    }
 public:
-    BinTree(T val){
-        root->left = nullptr;
-        root->right = nullptr;
-        root->value = val;
+    NodeTree* AddTo(T element) {
+        if (root == NULL) {
+            root = GetNewNode(element);
+            sizeTree++;
+        } else if (element <= root->data) {
+            root->left = ToAdder(root->left, element);
+        } else {
+            root->right = ToAdder(root->right, element);
+        }
+        return root;
+    }
+    int Size(){
+        return this->sizeTree;
     }
 
-    BinTree(){
-        root->left = nullptr;
-      root->right = nullptr;
-      root->value = nullptr;
-   }
-
-
-
-
-    void add(T val){
-        if(&root->value == nullptr)
-           root = new Node(val);
-        else
-            AddTo(*root,val);
+    void show() {
+        if (this->root != NULL) {
+            shower(root->left);
+            el.push_back(root->data);
+            cout<<root->data << endl;
+            shower(root->right);
+        }
     }
 };
 
+
 int main() {
-    int a;
-    BinTree<int> f(5);
-    while(true){
-        cin >> a;
-        f.add(a);
-        if(a == 0)
-            break;
-    }
+    Tree<int> tree1;
+    Tree<string> tree2;
+    tree1.AddTo(92);
+    tree1.AddTo(5);
+    tree1.AddTo(17);
+    tree1.AddTo(3);
+    tree1.AddTo(2);
+    tree1.AddTo(1);
+    tree1.AddTo(323232);
+    tree1.show();
+    
+    tree2.AddTo("Hello");
+    tree2.AddTo("World");
+    tree2.AddTo("!");
+    tree2.show();
 
     return 0;
 }
