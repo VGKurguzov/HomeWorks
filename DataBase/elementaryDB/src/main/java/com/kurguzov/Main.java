db2d50723069929192af13982c51bbe59bb666c9
saxakiil <vovakurguzov1999@yandex.ru>
2019-10-11 01:14:50 +0300

    Update project: elementaryDB

--- DataBase/elementaryDB/src/main/java/com/kurguzov/Main.java
+++ DataBase/elementaryDB/src/main/java/com/kurguzov/Main.java
@@ -11,16 +11,20 @@ public class Main {
         String name = "sa";
         String password = "";
 
-
         try{
             Class.forName("org.h2.Driver");
             connection = DriverManager.getConnection(url,name,password);
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM SUBSCRIBERS");
+            ResultSet resultSet = statement.executeQuery("SELECT distinct SUB_TARIFF,SUB_PRICE FROM SUBSCRIBERS ORDER BY SUB_PRICE");
 
             while(resultSet.next()){
+                System.out.println(resultSet.getString("sub_tariff") + " - " + resultSet.getInt("sub_price"));
+            }
+            System.out.println("--------------------------------------------------------");
+            resultSet = statement.executeQuery("SELECT SUB_ID, SUB_FIO, CITY_NAME, SUB_TARIFF, SUB_PRICE FROM SUBSCRIBERS LEFT JOIN CITIES ON SUB_CITY_ID = CITY_ID");
+            while(resultSet.next()){
                 System.out.println("#" + resultSet.getInt("sub_id")
-                +"\t" + resultSet.getString("sub_fio") + "\t" + resultSet.getString("sub_city")+
+                        +"\t" + resultSet.getString("sub_fio") + "\t" + resultSet.getString("city_name")+
                         "\t" + resultSet.getString("sub_tariff") + "\t" + resultSet.getInt("sub_price"));
             }
 
