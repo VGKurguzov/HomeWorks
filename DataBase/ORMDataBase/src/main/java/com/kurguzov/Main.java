14de378b2fb6615c6315b52a27b022f6079da306
saxakiil <vovakurguzov1999@yandex.ru>
2019-10-11 00:55:04 +0300

    Update project:ORMDataBase

--- DataBase/ORMDataBase/src/main/java/com/kurguzov/Main.java
+++ DataBase/ORMDataBase/src/main/java/com/kurguzov/Main.java
@@ -1,6 +1,5 @@
 package com.kurguzov;
 
-import com.kurguzov.hibernate.CitiesEntity;
 import com.kurguzov.hibernate.SubscribersEntity;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
@@ -13,13 +12,22 @@ public class Main {
         Session session = HibernateSessionFactory.getSessionFactory().openSession();
         session.beginTransaction();
 
-        String hql="from SubscribersEntity c";
-        Query<SubscribersEntity> query = session.createQuery(hql);
-
+        Query<SubscribersEntity> query = session.createQuery("from SubscribersEntity c");
         List<SubscribersEntity> cities = query.getResultList();
-
+        System.out.println("Места жительства абонентов:");
         for (SubscribersEntity city : cities) {
-            System.out.println(city.getSubFio()+"---" + city.getSubCityId().getCityName());
+            System.out.println(city.getSubFio()+" живёт в городе" + city.getSubCityId().getCityName());
+        }
+
+        System.out.println("Общие данные о абонентах:");
+        for (SubscribersEntity city : cities) {
+            System.out.println(city.getSubFio()+ " - " + city.getSubTariff()+ " - "
+                    + city.getSubPrice() + " рублей" + " - " +city.getSubCityId().getCityName());
+        }
+
+        System.out.println("\t Информация об оплате каждого абонента:");
+        for (SubscribersEntity city : cities) {
+            System.out.println(city.getSubFio()+ " платит по " + city.getSubPrice() + " рублей");
         }
 
         session.close();
