5e695c0d4fbf6468c6c75e2c469c5ba10aa67c2f
saxakiil <vovakurguzov1999@yandex.ru>
2020-01-19 00:09:36 +0300

    Update project DAO+singleTable: fixing bugs

--- DataBase/DAO+singleTable/src/main/java/com/kurguzov/DAO/SubscribersDAO.java
+++ DataBase/DAO+singleTable/src/main/java/com/kurguzov/DAO/SubscribersDAO.java
@@ -1,17 +1,13 @@
 package com.kurguzov.DAO;
 
 import com.kurguzov.Entities.SubscribersEntity;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
 public interface SubscribersDAO {
-    void add(SubscribersEntity sub)throws SQLException,ClassNotFoundException,SAXException,IOException,ParserConfigurationException;
-    SubscribersEntity getById(int id)throws SQLException,ClassNotFoundException,SAXException,IOException,ParserConfigurationException;
-    void update(SubscribersEntity sub)throws SQLException,ClassNotFoundException,SAXException,IOException,ParserConfigurationException;
-    void delete(int id)throws SQLException,ClassNotFoundException,SAXException,IOException,ParserConfigurationException;
-    List<SubscribersEntity> getAll()throws SQLException,ClassNotFoundException,SAXException,IOException,ParserConfigurationException;
+    void add(SubscribersEntity sub)throws SQLException,ClassNotFoundException;
+    SubscribersEntity getById(int id)throws SQLException,ClassNotFoundException;
+    void update(SubscribersEntity sub)throws SQLException,ClassNotFoundException;
+    void delete(int id)throws SQLException,ClassNotFoundException;
+    List<SubscribersEntity> getAll()throws SQLException,ClassNotFoundException;
 }
